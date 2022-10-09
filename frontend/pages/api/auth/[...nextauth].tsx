ac3a4494425f2cec2dd2387a5ccb69134afad71c
tonykogias <tonykogias@gmail.com>
2022-10-10 02:10:01 +0300

    chore: update next auth

--- frontend/pages/api/auth/[...nextauth].tsx
+++ frontend/pages/api/auth/[...nextauth].tsx
@@ -1,11 +1,12 @@
 /* External Imports */
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
+import {GithubProfile} from "next-auth/providers/github";
 
 export default NextAuth({
   providers: [
     // Github OAuth provider
-    GitHub({
+    GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
     }),
@@ -26,13 +27,14 @@ export default NextAuth({
   },
   callbacks: {
     // Running when use signin/signout
-    async jwt({ token, user }) {
+    async jwt({ token, user, profile }) {
       const isSignIn = !!user;
       if (isSignIn) {
+        const _profile: GithubProfile = profile as GithubProfile;
         token.github_id = user?.id;
-        token.github_name = user?.login;
-        token.github_following = user?.following;
-        token.github_created_at = user?.created_at;
+        token.github_name = _profile?.login;
+        token.github_following = _profile?.following;
+        token.github_created_at = _profile?.created_at;
       }
 
       // Resolve JWT
@@ -41,13 +43,14 @@ export default NextAuth({
 
     // Running when we want to retrieve the session e.g. when calling `getSession()`
     async session({ session, token }) {
+      const _session = session as any;
       // Attach additional params from JWT to session
-      session.github_id = token.github_id;
-      session.github_name = token.github_name;
-      session.github_following = token.github_following;
-      session.github_created_at = token.github_created_at;
+      _session.github_id = token.github_id;
+      _session.github_name = token.github_name;
+      _session.github_following = token.github_following;
+      _session.github_created_at = token.github_created_at;
       // Resolve session
-      return Promise.resolve(session);
+      return Promise.resolve(_session);
     },
   },
 });
